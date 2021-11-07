b9a4fd3067e5a2ad6fb4999c15347afdcdb2438b
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-07 12:41:20 -0600

    includes working get, post, delete, and put but delete and put are not perfect

--- client/newClient.js
+++ client/newClient.js
@@ -7,9 +7,7 @@ const groceryListItem = (groceryListItem) => {
     let list = document.createElement('li')
     list.classList.add('grocery-list-item')
 
-    list.innerHTML = `<span>${groceryListItem.item}</span>`
-
-    //display each item in the json node array as list items in the unordered list element on the page
+    list.innerHTML = `<button onclick="deleteItem(${groceryListItem.id})">X</button><span>${groceryListItem.item}</span><button onclick="updateQuantity(${groceryListItem.id},'minus')">-</button><span class="item-qty">${groceryListItem.quantity}</span><button onclick="updateQuantity(${groceryListItem.id},'plus')">+</button>`
 
     groceryContainer.appendChild(list)
 }
@@ -21,7 +19,7 @@ groceryButton.addEventListener('click', () => {
         axios.get(baseURL)
             .then( (response) => {
                 let groceryArray = response.data
-
+                groceryContainer.innerHTML = ``
                 for (i=0; i<groceryArray.length; i++) {
                     groceryListItem(groceryArray[i])
                 }
@@ -36,9 +34,23 @@ const createListItem = body => axios.post(baseURL, body).then(groceryListItem)
 
 const addButton = document.querySelector('#add')
 addButton.addEventListener('click', () => {
-    let item = document.querySelector('input')
-    let bodyObj = {item: item.value}
+    let item = document.querySelector('#input')
+    let quantity = document.querySelector('#qty')
+    let bodyObj = {item: item.value,
+                   quantity: quantity.value
+                }
 
     createListItem(bodyObj)
 }); 
+
+//The DELETE
+//*MUST CLICK TO SEE GROCERY LIST FOR UPDATE*/
+const deleteItem = (id, event) => {
+    axios.delete(`${baseURL}/${id}`)
+    .then(groceryListItem)
+}
+
+//The PUT
+//**THIS IS BUGGY BUT ALSO MUST CLICK TO SEE GROCERY LIST FOR UPDATE */
+const updateQuantity = (id, type) => axios.put(`${baseURL}/${id}`, {type}).then(groceryListItem)
 
