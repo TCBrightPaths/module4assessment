35aa25ecffa7ae62cc486c6b7e00b9d63fb9b186
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-07 12:44:42 -0600

    up to date

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
@@ -21,5 +22,29 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["The crying baby gets the milk.",
+					 "The palest ink is better than the sharpest sword.",
+					 "It is better to light a candle than to curse the darkness.",
+  ];
+
+  // choose random fortune
+  let randomIndex2 = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex2];
+
+  res.status(200).send(randomFortune);
+  
+});
+
+const { getGroceries, addGroceries, deleteGroceries, updateGroceries } = require('./controller.js');
+
+app.get(`/api/groceries`, getGroceries);
+app.post(`/api/groceries`, addGroceries);
+app.put(`/api/groceries/:id`, updateGroceries);
+app.delete(`/api/groceries/:id`, deleteGroceries);
+ 
+
+  
 
 app.listen(4000, () => console.log("Server running on 4000"));
